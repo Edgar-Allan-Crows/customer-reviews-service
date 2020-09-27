8fbe01818bb6fef48b4b8bf93c07547d78ef21b2
Chris Cali <ChristopherCali1415@gmail.com>
2020-09-27 18:42:52 -0400

    Database seeded by running schema.sql script in mysql terminal and seed.js in node terminal

--- database/seed.js
+++ database/seed.js
@@ -10,31 +10,46 @@ const connection = mysql.createConnection({
   database: "fec_reviews"
 });
 
-// Use faker to create test data
-var username_fake = faker.name.firstName();
-var fecha_fake = faker.date.past();
-var formattedDate = JSON.stringify(fecha_fake).split('T')[0].split('"').join('')
-var rating_fake = Math.floor(Math.random() * (5 - 1) + 1);
-var title_fake = faker.random.words();
-var review_body_fake = faker.lorem.paragraph();
-
+// Populate reviews table
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Database connected as expected in seed.js!');
-  // Query the database with one unique entry by utilizing faker
-  let entry = `INSERT INTO reviews (username, fecha, rating, title, review_body) VALUES ("${username_fake}", "${formattedDate}", "${rating_fake}", "${title_fake}", "${review_body_fake}")`;
-  connection.query(entry, (err, result) => {
-    if (err) throw err;
-    console.log('1 record inserted');
-    // Close database connection
-    connection.end();
-  });
+  console.log('Database connected to populate reviews table!');
+
+  let reviews_count = 0;
+  let images_count = 0;
+
+  while (reviews_count < 100) {
+    // Use faker to create test data
+    var username_fake = faker.name.firstName();
+    var fecha_fake = faker.date.past();
+    var formattedDate = JSON.stringify(fecha_fake).split('T')[0].split('"').join('')
+    var rating_fake = Math.floor(Math.random() * (5 - 1) + 1);
+    var title_fake = faker.random.words();
+    var review_body_fake = faker.lorem.paragraph();
+
+    // Populate reviews table
+    let entry = `INSERT INTO reviews (username, fecha, rating, title, review_body) VALUES ("${username_fake}", "${formattedDate}", "${rating_fake}", "${title_fake}", "${review_body_fake}")`;
+    connection.query(entry, (err, result) => {
+      if (err) throw err;
+      console.log('1 reviews table record inserted');
+    });
+    reviews_count++;
+  }
+
+  while (images_count < 100) {
+    var image_review_id = Math.floor(Math.random() * (100 - 1) + 1);
+    var image_url_fake = faker.image.imageUrl();
+    // Populate images table
+    let images_entry = `INSERT INTO images (image_url, review_id) VALUES ("${image_url_fake}", "${image_review_id}")`;
+    connection.query(images_entry, (err, result) => {
+      if (err) throw err;
+      console.log('1 images table record inserted');
+    });
+    images_count++;
+  }
+
+  // Close database connection
+  connection.end();
 });
 
-
-// Run it in the terminal
-// Check mysql to see if entry was created as expected
-// If yes, write script to do it 99 more times
-// If no, fix it
-
-// Then figure out S3 to host images there
+// Now figure out S3 to host images there
