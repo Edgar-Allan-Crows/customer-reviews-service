70a9be00156fe2632700620ddc57acde54c0d812
Chris Cali <ChristopherCali1415@gmail.com>
2020-09-28 20:15:28 -0400

    Refactor server/index.js to remove duplicate code

--- server/index.js
+++ server/index.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3004;
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password!',
-  database: 'fec_reviews'
-})
-
+const db = require('../database/index.js');
 
 app.get('/api', (req, res) => {
-  connection.connect();
-  connection.query('SELECT * FROM reviews', (err, result) => {
+  db.query('SELECT * FROM reviews', (err, result) => {
     if (err) throw err;
     res.send(result);
   });
-  connection.end();
+  db.end();
 })
 
 app.listen(port, () => {
