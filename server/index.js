10c34e6f0da267e0c66f5332f79921c77f522339
Chris Cali <ChristopherCali1415@gmail.com>
2020-09-29 00:29:41 -0400

    Add webpack script and jquery dep to package.json. Adjust file structure and add middleware to server to render static index.html

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3004;
 const db = require('../database/index.js');
 
+app.use(express.static(__dirname + '/../dist'));
+
 app.get('/api', (req, res) => {
   db.query('SELECT * FROM reviews', (err, result) => {
     if (err) throw err;
