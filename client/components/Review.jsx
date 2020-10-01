89871c219af6a393e5539d29c26c447633ec24bf
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-01 10:19:51 -0400

    Ratings images rendering as expected

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { formatDate } from '../utils/helpers.js'
+import fiveStars from '../img/5-stars.png';
+import fourStars from '../img/4-stars.png';
+import threeStars from '../img/3-stars.png';
+import twoStars from '../img/2-stars.png';
+import oneStar from '../img/1-star.png';
 
 const SingleReview = styled.div`
   display: flex;
@@ -16,7 +21,21 @@ const Review = ({review}) => (
     <div className="reviewDetails">
       <div className="nameCity">{review.username}</div>
       <div className="nameCity">City, State</div>
-      <div>Rating: {review.rating} / 5</div>
+
+        {[review.rating].map((rating, key) => {
+          if (rating === 5) {
+            return <img key={key} id="ratingImg" src={fiveStars} />
+          } else if (rating === 4) {
+            return <img key={key} id="ratingImg" src={fourStars} />
+          } else if (rating === 3) {
+            return <img key={key} id="ratingImg" src={threeStars} />
+          } else if (rating === 2) {
+            return <img key={key} id="ratingImg" src={twoStars} />
+          } else if (rating === 1) {
+            return <img key={key} id="ratingImg" src={oneStar} />
+          }
+        })}
+
       <div id="reviewTitle">{review.title}</div>
       <p>{review.review_body}</p>
     </div>
