a3389470f56539b0f0d731deaf28626e7003841f
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-02 12:37:42 -0400

    Add babel configurations to package.json

--- spec/src/App.test.js
+++ spec/src/App.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../client/App.jsx';
 
-import "babel-polyfill";
+const wrapper = shallow(<App />)
 
-describe('First React component test with Enzyme', () => {
-  it('App renders without crashing', () => {
-     shallow(<App />);
-   });
-});
+// describe('First React component test with Enzyme', () => {
+//   it('App renders without crashing', () => {
+//      shallow(<App />);
+//    });
+// });
