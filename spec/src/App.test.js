5ef433f84691db1f7953ebce6b556e242035481a
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-02 01:05:51 -0400

    Exporting App component from client/App.jsx, cleaned up dependencies and devDependencies (added a few) to package.son, wrote first front-end test (not passing), and added a preset to webpack

--- spec/src/App.test.js
+++ spec/src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../client/App.jsx';
 
+import "babel-polyfill";
+
 describe('First React component test with Enzyme', () => {
   it('App renders without crashing', () => {
      shallow(<App />);
