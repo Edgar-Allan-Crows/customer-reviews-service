c726b548c1141485965462db572eb2c4f552de1d
Charlie Sellers <CharlieJSellers@gmail.com>
2020-10-06 13:20:32 -0500

    Changed 'app.jsx' to 'App.jsx'

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, '/client/app.jsx'),
+  entry: path.join(__dirname, '/client/App.jsx'),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js'
