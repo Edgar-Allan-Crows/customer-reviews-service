e1745fe59a28ed9cbb3c36d7d02d2352f0849f74
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-08 17:34:09 -0400

    Add 100 products in a new table called products and add a random amount of reviews to each product via reviews table

--- database/schema.sql
+++ database/schema.sql
@@ -2,24 +2,31 @@ CREATE DATABASE IF NOT EXISTS fec_reviews;
 
 USE fec_reviews;
 
+CREATE TABLE IF NOT EXISTS products (
+  product_id int unsigned NOT NULL,
+  PRIMARY KEY (product_id)
+);
+
 CREATE TABLE IF NOT EXISTS reviews (
   review_id INT unsigned NOT NULL AUTO_INCREMENT,
+  product_id INT unsigned NOT NULL,
   username VARCHAR(150) NOT NULL,
   fecha DATE NOT NULL,
   city_state text NOT NULL,
   rating INT(5) NOT NULL,
   title varchar(150) NOT NULL,
   review_body text NOT NULL,
-  PRIMARY KEY (review_id)
+  PRIMARY KEY (review_id),
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
-CREATE TABLE IF NOT EXISTS images (
-  image_id INT unsigned NOT NULL AUTO_INCREMENT,
-  image_url VARCHAR(500) NOT NULL,
-  review_id INT unsigned NOT NULL,
-  PRIMARY KEY (image_id),
-  FOREIGN KEY (review_id) REFERENCES reviews(review_id)
-);
+-- CREATE TABLE IF NOT EXISTS images (
+--   image_id INT unsigned NOT NULL AUTO_INCREMENT,
+--   image_url VARCHAR(500) NOT NULL,
+--   review_id INT unsigned NOT NULL,
+--   PRIMARY KEY (image_id),
+--   FOREIGN KEY (review_id) REFERENCES reviews(review_id)
+-- );
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < database/schema.sql -p
